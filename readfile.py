59bdf62cbb1269a26a51bce66464aec3fb7ee4fc
Andrei Darien Elliott <a.d.elliott@ku.edu>
2015-04-06 03:32:54 -0500

    database file parsing function

---
+++ readfile.py
@@ -0,0 +1,19 @@
+#/bin/python
+import Hmwk_4_books_containers_and_obj as obj
+
+def buildDBfromFile(filename):
+    file = open(filename)
+    db = obj.books_container()
+    category = False
+    price = False
+    for line in file:
+        if line[0] == "-": # book entry
+            [author, title] = line[2:].split(", ", 1) # drop "- ", then split at the comma
+            if not category:
+                raise ValueError("Database file could not be parsed.")
+            else:
+                db.newBook(category, title, author, price)
+        else: # we are reading a section heading
+            category = line[:line.find(" Books")]
+            price = int(line[line.find("$")+1:].split(" ")[0]) # ugly hack based on db file layout - goes from after the first '$' to the following space
+    return db
